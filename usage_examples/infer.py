9021571d91ce49eca85ff48c6198443d27f8db0c
Maxime Moreillon <m.moreillon@gmail.com>
2023-04-06 16:20:44 +0900

    sampel inference code

---
+++ usage_examples/infer.py
@@ -0,0 +1,17 @@
+import requests
+
+IMAGE_PATH = './example.jpg'
+URL = 'http://localhost:7071/predict'
+
+files = { 'image' : ('myImage.jpg', open(IMAGE_PATH,'rb').read()) }
+
+# Send the image
+print(f'Infering from to {URL}')
+response = requests.post(URL, files=files)
+
+# Check if inference is successful
+if response.status_code ==  200:
+    print(response.json())
+else:
+    print(f'Inference failed with code {response.status_code}')
+    print(response.text)
